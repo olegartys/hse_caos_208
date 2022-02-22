38e5dce8dc65f4ae9531055da9d2750d2a138dd2
olegartys <olegartys@gmail.com>
2022-02-22 19:26:30 +0300

    Introduce sem13_fork material
    Signed-off-by: olegartys <olegartys@gmail.com>

---
+++ sem13_fork/fork_wait/fork_wait.c
@@ -0,0 +1,36 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void) {
+	pid_t p = fork();
+	switch (p) {
+		case -1:
+			perror("fork");
+			break;
+
+		case 0:
+			printf("I am in child, pid=%d, ppid=%d\n", getpid(), getppid());
+			return 10;
+
+		default: {
+			printf("I am in parent, waiting for child %d to finish\n", p);
+
+			int status;
+			if (waitpid(p, &status, 0) == -1) {
+				perror("waitpid");
+				return 1;
+			}
+
+			if (!WIFEXITED(status)) {
+				printf("Child exited with non-zero status\n");
+				return 1;
+			}
+
+			printf("Child (%d) exited with status %d\n", p, WEXITSTATUS(status));
+
+			return 0;
+		}
+	}
+}
