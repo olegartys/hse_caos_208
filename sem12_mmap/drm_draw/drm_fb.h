dfa607ec0c205de07a9a718c617a1f21653be1f5
olegartys <olegartys@gmail.com>
2022-02-23 10:39:35 +0300

    sem12: fix indentation
    Signed-off-by: olegartys <olegartys@gmail.com>

--- sem12_mmap/drm_draw/drm_fb.h
+++ sem12_mmap/drm_draw/drm_fb.h
@@ -6,22 +6,22 @@
 #include <xf86drmMode.h>
 
 struct drm_fb {
-	int fd;
+    int fd;
 
-	int buf_id;
+    int buf_id;
     int crtc_id;
     int connector_id;
 
     size_t size;
     off_t offset;
 
-	drmModeResPtr res;
+    drmModeResPtr res;
     drmModeConnectorPtr connector;
     drmModeEncoderPtr encoder;
     drmModeModeInfoPtr preferred_mode;
 };
 
 struct drm_fb *drm_fb_init(int fd, int width, int height, int bpp, int depth,
-	int connector_type, int connector_type_id);
+    int connector_type, int connector_type_id);
 int drm_draw(struct drm_fb *);
 void drm_fb_release(struct drm_fb *);
